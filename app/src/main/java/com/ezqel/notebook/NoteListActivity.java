0381fa23c45e99a0d5662cb5ed19219cad2d2d17
ezQel <ezekiel3499@gmail.com>
2019-09-01 10:58:45 +0300

    Migrated to androidx

--- app/src/main/java/com/ezqel/notebook/NoteListActivity.java
+++ app/src/main/java/com/ezqel/notebook/NoteListActivity.java
@@ -2,19 +2,13 @@ package com.ezqel.notebook;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
 import java.util.List;
 
--- app/src/main/java/com/ezqel/notebook/NoteRecyclerAdapter.java
+++ app/src/main/java/com/ezqel/notebook/NoteRecyclerAdapter.java
@@ -2,9 +2,8 @@ package com.ezqel.notebook;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
